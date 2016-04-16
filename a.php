929fbe96200ed82758ac0c2021eb3b5e624494f3
xcwen <xcwenn@qq.com>
2016-04-16 15:13:48 +0800

    fix @method

--- a.php
+++ a.php
@@ -6,9 +6,14 @@ class Testb  extends Facade {
     /**
      * @return $this
      */
+
     public function get_v8(){
         //$this->v1
     }
     use Instance;
+
+    use Concerns\InteractsWithContainer,
+        Concerns\MakesHttpRequests,
+        Concerns\ImpersonatesUsers;
 
 }
